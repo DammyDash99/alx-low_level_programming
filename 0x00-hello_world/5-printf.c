9d773a250a5c46bef2082a07157836ff64e7ee33
Shonubi Damilola <shonubidamilola@gmail.com>
2023-03-16 17:47:09 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * printf - prints string characters
+ *
+ * return - if no error,value is 0
+ */
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
