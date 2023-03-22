d85cd5201377bd895a9a78297180cb8bb7ed669d
Shonubi Damilola <shonubidamilola@gmail.com>
2023-03-22 14:59:22 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - main function
+ *
+ * @a: one of the values
+ *
+ * @b: another value
+ *
+ * Return: returns result of the operation
+ *
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return result;
+}
