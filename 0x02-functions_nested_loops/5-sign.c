194cb8c742953e63ec2db606268c3b59619ff379
Shonubi Damilola <shonubidamilola@gmail.com>
2023-03-22 08:07:45 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - main function
+ *
+ * @n: character to be checked
+ *
+ * Return: returns possitve when >,0 when equal toand negative when <
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return(1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return(0);
+	}
+	else
+	{
+		_putchar('_');
+		return(-1);
+	}
+}
