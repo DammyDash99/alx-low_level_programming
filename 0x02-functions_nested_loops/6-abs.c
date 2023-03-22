8467ec927d23b091b451d2900e906937aca48a8c
Shonubi Damilola <shonubidamilola@gmail.com>
2023-03-22 08:42:01 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - main function
+ *
+ * @n: charactet to be checked
+ *
+ * Return: returns 0
+ *
+ */
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (n * -1);
+}
