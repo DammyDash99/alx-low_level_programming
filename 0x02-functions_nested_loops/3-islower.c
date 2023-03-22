763a644167a16d85ab3fedc25bcd85e6b23acc1a
Shonubi Damilola <shonubidamilola@gmail.com>
2023-03-22 05:35:58 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - checks for lower case character
+ *
+ * @c- charcter to be checked
+ *
+ * Return: returns 1 if operation is true,0 of false
+ *
+ */
+int _islower(int c);
+{
+	if (c => 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
