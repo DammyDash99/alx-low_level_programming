513c2d17312763eccfb41c6e14c47979f28637a4
Shonubi Damilola <shonubidamilola@gmail.com>
2023-04-15 15:52:22 -0700

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,8 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
-{
-	printf("%s\n", argv[argc * 0]);
-
-	return (0);
+int main(int argc __attribute__((unused)), char *argv[])
+{                
+        printf("%s\n", *argv);
+        return (0);
 }
